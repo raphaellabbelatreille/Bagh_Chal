eda31ac1004f517ccbe99c920b74ad46b547a67e
raphaellabbelatreille <skybirds4545@gmail.com>
2025-05-08 14:15:21 -0400

    Ajustement de taile et ajour de bordurer au éélément

--- game/Theme.js
+++ game/Theme.js
@@ -2,8 +2,14 @@
 export class Theme {
     constructor() {
         this.refRoot = document.querySelector(":root")
-        this.listOfVariable = ["--couleurFondDEcran", "--couleurFondBoard","--couleurNode","--couleurFond","--couleurFondBulle", "--couleurTexte", "--couleurCursor", "--couleurProie", "--couleurPredateur"]
-        this.listOfThemeAvailable = ["default", "grass", "midnight", "syrup"]
+        this.listOfVariable = ["--couleurFondDEcran", "--couleurFondBoard","--couleurNode","--couleurFond","--couleurFondBulle", "--couleurTexte","--couleurBorder", "--couleurCursor", "--couleurProie", "--couleurPredateur"]
+        this.listOfThemeAvailable = ["Default", "Grass", "Midnight", "Syrup", "Neon"]
+
+        this.listOfDefaultColor = ["rgb(255, 255, 255)","rgb(70, 10, 70)","rgb(255, 255, 255)","rgb(200, 205, 200) ","rgb(180,180,180)", "rgb(0, 0, 0)","rgba(0, 0, 0, .2)","rgb(255,225,130)","rgb(100,150,255)",  "rgb(255,225,130)"]
+        this.listOfGrassColor = ["rgb(0, 77, 46)","rgb(55,45,40)","rgb(180, 180, 180)","rgb(0, 96, 57)","#616161", "rgb(255, 255, 255)","rgba(255,255,255,.8)","rgb(255,225,130)", "__", "__"]
+        this.listOfMidnightColor = ["rgb(0, 10, 46)","rgb(0, 30, 100)","rgb(180, 180, 180)","rgb(20, 10, 70)","#616161", "rgb(255, 255, 255)","rgba(255,255,255,.8)","rgb(255,225,130)", "__", "__"]
+        this.listOfSyrupColor = ["rgb(255,250,210)","rgb(78, 10, 0)","rgb(226, 226, 226)","rgb(203, 160, 150)","rgb(233, 150, 80)", "rgb(0, 0, 0)","rgba(255,230,190, .9)","rgb(255,225,130)", "__", "__"]
+        this.listOfNeonColor = ["rgb(2,0,33)","rgb(30, 30, 50)","rgb(239, 116, 110)","rgb(30, 30, 50)","rgb(141, 215, 252)", "rgb(255, 255, 255)","rgba(255,255,255,.8)","rgb(255,225,130)", "rgb(100,250,155)", "__"]
         this.prepareTheBtnToSwitchTheme();
         if (localStorage.getItem("ThemeSelectionner") != ""){
             this.takeNewRequest(localStorage.getItem("ThemeSelectionner"))
@@ -15,25 +21,39 @@ export class Theme {
         for (let indexTheme = 0; indexTheme < this.listOfThemeAvailable.length; indexTheme++) {
             let themeName = this.listOfThemeAvailable[indexTheme];
             /*this["refBtn"+themeName]*/
-            document.getElementById(themeName).addEventListener("click", this.takeNewRequest.bind(this, themeName))
+            let themeBtn = document.getElementById(themeName)
+            for (let indexVar = 0; indexVar < (this.listOfVariable.length-3); indexVar++) {
+                //let curentVariable = this.listOfVariable[indexVar];
+                let newColor = this["listOf"+themeName+"Color"][indexVar]
+                if (newColor == "__"){
+                    newColor = this.listOfDefaultColor[indexVar]
+                }
+                let newSquare = document.createElement("div")
+                newSquare.style = "width:20px ;height:20px ; border : 1px solid black ; background-color: "+newColor;
+                themeBtn.appendChild(newSquare)  
+            }
+
+            themeBtn.addEventListener("click", this.takeNewRequest.bind(this, themeName))
         }
     }
     //"rgb(105, 35, 80)"
     takeNewRequest(request){
-        this.listOfDefaultColor = ["rgb(200, 200, 250)","rgb(105, 35, 80)","rgb(180, 180, 180)","rgb(180,180,180) ","rgb(180, 120, 170)", "rgb(0, 0, 0)","rgb(255,225,130)","rgb(100,150,255)",  "rgb(255,225,130)"]
         switch(request){
-            default: case "default":
-                request = "default"
+            default: case "Default":
+            request = "Default"
                 this.applyValue(this.listOfDefaultColor)
                 break;
-            case "midnight":
-                this.applyValue(["rgb(0, 10, 46)","rgb(0, 30, 100)","rgb(180, 180, 180)","rgb(20, 10, 70)","#616161", "rgb(255, 255, 255)","rgb(255,225,130)", "__", "__"])
+            case "Midnight":
+                this.applyValue(this.listOfMidnightColor)
                 break;
-            case "grass":
-                this.applyValue(["rgb(0, 77, 46)","rgb(55,45,40)","rgb(180, 180, 180)","rgb(0, 96, 57)","#616161", "rgb(255, 255, 255)","rgb(255,225,130)", "__", "__"]);
+            case "Grass":
+                this.applyValue(this.listOfGrassColor);
                 break;
-            case "syrup":
-                this.applyValue(["rgb(0,0,0)","rgb(55,45,40)","rgb(226, 226, 226)","rgb(255, 0, 0)","#616161", "rgb(255, 255, 255)","rgb(255,225,130)", "__", "__"]);
+            case "Syrup":
+                this.applyValue(this.listOfSyrupColor);
+                break;
+            case "Neon":
+                this.applyValue(this.listOfNeonColor);
                 break;
             
         }  
